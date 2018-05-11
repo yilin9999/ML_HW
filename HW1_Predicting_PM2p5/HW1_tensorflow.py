f4db71a55f08aba6ad563a7f23b95e5e4a0a4ad5
yilin9999 <37095498+yilin9999@users.noreply.github.com>
2018-05-11 23:40:28 +0800

    keras ok

--- HW1_Predicting_PM2p5/HW1_tensorflow.py
+++ HW1_Predicting_PM2p5/HW1_tensorflow.py
@@ -7,10 +7,15 @@ Created on Wed May  9 23:21:52 2018
 import csv 
 import numpy as np
 import tensorflow as tf
+
+import keras
 from keras.models import Sequential
 from keras.layers import Dense
 from keras import optimizers
-
+from HW1_test import test_routing
+from keras.utils import plot_model
+import sys
+from time import time
 
 testFile  = "./test.csv"
 trainFile = "./train.csv"
@@ -49,6 +54,7 @@ trainY = np.zeros(trainCnt)
 c     = 0
 start = 0
 
+############# Read TrainXY
 for i in range(12):
     start = i * (24*20)
     for j in range(mUnit):
@@ -60,24 +66,102 @@ for i in range(12):
         c += 1
         start += 1
 
-trainX = np.concatenate(
-        (trainX, np.ones((len(trainX), 1), dtype=float)), 
-        axis=1)
+#trainX = np.concatenate( \
+#        (trainX, np.ones((len(trainX), 1), dtype=float)),  \
+#        axis=1)
+
+############# Read TestX
+#Read Testing file
+fptr  = open(testFile, "r")
+frow  = csv.reader(fptr, delimiter=",")
 
 
-epochNum = 100000
+tmpList = []
+rowCnt  = 0
+for irow in frow:    
+    if rowCnt%18==0:
+        tmpList.append([])
+        
+    for item in irow[2:11]:
+        #print(item)
+        if item == "NR":
+            tmpList[-1].append(float(0))
+        else:
+            tmpList[-1].append(float(item))
+
+    rowCnt += 1
+
+fptr.close()
+
+testX = np.array(tmpList)
+#testX = np.concatenate((testX, np.ones((len(testX),1))), axis=1)
+    
+############# Read TestY
+tmpList = []
+fptr  = open(ansFile, "r")
+frow  = csv.reader(fptr, delimiter=",")  
+
+fptr.readline() #skip the first line
+for irow in frow:     
+    tmpList.append(float(irow[1]))
+
+testY = np.array(tmpList)
+fptr.close()
+    
+ ########################################################################################### 
+
+
+#myWeight  = np.zeros(163) #include bias
 
 myModel = Sequential()
-myModel.add(Dense(output_dim=1, input_dim=163))
+myModel.add(Dense(1, input_dim=162, activation='linear'))
 
-#sgd = optimizers.SGD(lr=0.0001, decay=1e-6, momentum=0.9, nesterov=True)
-adam = optimizers.Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
-myModel.compile(loss='mse', optimizer=adam)
-#myModel.fit(trainX, trainY, nb_epoch=epochNum, batch_size=5620, verbose=0)
+#myModel.layers[0].set_weights(myWeight)
+#sys.exit(0)
+#myModel.add(Dense(input_dim=163, output_dim=1))
 
 
+#sgd = optimizers.SGD(lr=0.0001, decay=1e-6, momentum=0.5, nesterov=True)
+class LossHistory(keras.callbacks.Callback):
+    def on_batch_end(self, batch, logs={}):
+        self.seen += logs.get('size', 0)
+        if self.seen % self.display == 0:
+            # you can access loss, accuracy in self.params['metrics']
+            print('\n{}/{} - loss ....\n'.format(self.seen, self.params['nb_sample'])) 
+
+epochNum = 100000
+sgd = optimizers.SGD(lr=0.0001, decay=0.000, momentum=0.5, nesterov=None)
+adag = optimizers.Adagrad(lr=0.01)
+adam = optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.000, amsgrad=False)
+
+#myModel.compile(loss='mse', optimizer=adam)
+#myModel.fit(trainX, trainY, nb_epoch=epochNum, batch_size=5620, verbose=1)
 #Training
-for i in range(100000):
-    Loss = myModel.train_on_batch(trainX, trainY)
-    if i%1000 == 0:
-        print("%d interation: Loss=%.4f" %(i, Loss))    
+
+history = LossHistory()
+myModel.compile(loss='mse', optimizer=adam, metrics=['accuracy'])
+
+beginTime = time()
+myModel.fit(trainX, trainY, nb_epoch=epochNum, batch_size=20, verbose=1)
+#for i in range(epochNum):
+#    Loss = myModel.train_on_batch(trainX, trainY)
+#    if i%500 == 0:
+#        print("%d interation: Loss=%.4f" %(i, np.sqrt(Loss)))    
+       
+endTime = time()
+
+Loss = myModel.evaluate(trainX, trainY, batch_size=5620)
+
+RMSE = np.sqrt(Loss)
+print("Loss=%.4f" %(RMSE))    
+print("Training Time: %.4f sec" %(endTime - beginTime))
+
+score  = myModel.evaluate(testX, testY, batch_size=240)
+w, b = myModel.layers[0].get_weights()
+print("Eout=%.4f" %(np.sqrt(score)))   
+
+ww = np.ndarray.flatten(w) 
+ww = np.concatenate((ww,b))
+test_routing(ww, testFile, ansFile, RMSE)
+ 
+
