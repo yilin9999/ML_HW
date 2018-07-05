abdcca2056110bf802f1b88a250b95097af09ebf
yilin9999 <37095498+yilin9999@users.noreply.github.com>
2018-07-05 17:43:01 +0800

    update

--- hw5_movie_recommendation/main.py
+++ hw5_movie_recommendation/main.py
@@ -13,23 +13,26 @@ from keras import layers
 from keras import callbacks
 #from keras.layers import Input
 from keras.utils import plot_model
+from keras.constraints import non_neg
+
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
 import sys
+import time
 
 def gen_model(n_users, n_items, latent_dim):
     
     userInputLayer = layers.Input(shape=[1])
     itemInputLayer = layers.Input(shape=[1])
     
-    userVec   = layers.Embedding(n_users, latent_dim, embeddings_initializer='random_normal', name='User_Embedding')(userInputLayer)
+    userVec   = layers.Embedding(n_users, latent_dim, embeddings_initializer='random_normal', name='User_Embedding', embeddings_constraint=non_neg())(userInputLayer)
     userBias  = layers.Embedding(n_users, 1, embeddings_initializer='zeros')(userInputLayer)
     
     userVec   = layers.Flatten()(userVec)    
     userBias  = layers.Flatten()(userBias)    
     
-    itemVec   = layers.Embedding(n_items, latent_dim, embeddings_initializer='random_normal', name='Movie_Embedding')(itemInputLayer)
+    itemVec   = layers.Embedding(n_items, latent_dim, embeddings_initializer='random_normal', name='Movie_Embedding', embeddings_constraint=non_neg())(itemInputLayer)
     itemBias  = layers.Embedding(n_items, 1, embeddings_initializer='zeros')(itemInputLayer)
     
     itemVec   = layers.Flatten()(itemVec)    
@@ -44,7 +47,7 @@ def gen_model(n_users, n_items, latent_dim):
     
     model   = models.Model(inputs=[userInputLayer, itemInputLayer], outputs=r_hat)
     model.summary()
-    model.compile(loss='mse', optimizer='sgd')
+    model.compile(loss='mse', optimizer='adam')
     
     plot_model(model, to_file='tmp/model.png', show_shapes=True, show_layer_names=True)  
     
@@ -52,18 +55,20 @@ def gen_model(n_users, n_items, latent_dim):
 
 def main(args):
     
-    
-    #paraDict = {'latent_dim': args['latent_dim'],
+        #paraDict = {'latent_dim': args['latent_dim'],
     #            'batch_size': 128}
     print(type(args))
     print(args)
     
     paraDict = args.__dict__
     print(paraDict)
-    #sys.exit(0)
     
-    trainX     = pd.read_csv(paraDict['train_path'], index_col=0)
-    testX      = pd.read_csv(paraDict['test_path'] , index_col=0)    
+    if paraDict['pred_only'] is True:        
+        predict(paraDict)        
+        sys.exit(1)
+        
+        
+    trainX     = pd.read_csv(paraDict['train_path'], index_col=0)    
     userDF     = pd.read_csv(paraDict['user_path'], index_col=None, delimiter='::')    
     movieDF    = pd.read_csv(paraDict['movie_path'], index_col=None, delimiter='::')   
     #allX       = pd.concat([trainX, testX], axis=0, join='outer')
@@ -71,15 +76,7 @@ def main(args):
     #userDict = dict( enumerate(trainX['UserID'].astype('category').cat.categories) )
     #print(trainX['UserID'].astype('category').cat.categories)
     
-    testX   = testX.head(1000)
-    #trainX = trainX.head(10)
-    #trainX = trainX.head(10)
-    userLE  = preprocessing.LabelEncoder()
-    movieLE = preprocessing.LabelEncoder()
-    #trainX['UserIdx']  = userLE.fit(trainX['UserID'])
-    #print(userLE.classes_)
-    userLE.fit(userDF['UserID'])
-    movieLE.fit(movieDF['movieID'])
+    userLE, movieLE = get_usr_movie_idx(paraDict['user_path'], paraDict['movie_path'])
     
     trainX['UserIdx']  = userLE.transform(trainX['UserID'])
     trainX['MovieIdx'] = movieLE.transform(trainX['MovieID'])
@@ -92,49 +89,24 @@ def main(args):
     
     """
     userDict  = dict(zip(userLE.classes_, userLE.transform(userLE.classes_)))
-    movieDict = dict(zip(movieLE.classes_,movieLE.transform(movieLE.classes_)))
-      
-    
-    new_user_idx  = n_user  - 1
-    new_movie_idx = n_movie - 1
+    movieDict = dict(zip(movieLE.classes_,movieLE.transform(movieLE.classes_)))        
     """
-
     #trainX['UserIdx']  = trainX['UserID'].astype('category').cat.codes.astype(np.int64)
     #trainX['MovieIdx'] = trainX['MovieID'].astype('category').cat.codes.astype(np.int64)
     
-    #print(trainX['UserID'].astype('category').cat.categories)
-    
-    #for idx, row in testX.iterrows():
-    #    trainX.loc[row['UserID']]
-    #for row in testX.head(10).iterrows():
-    #    testX[]
-    #    print(row)
-        #testX.loc['UserIdx']
-        #textX.loc['MovieIdx'][]
-
-    #trainX.sort_values(by=['MovieID'], inplace=True)    
-    #trainX = trainX.head(1000)
-    #trainMovie = trainMovie.head(1000)
     print(trainX.head(20))    
     print("User count  : %d"%n_user)    
     print("Movie count : %d"%n_movie)        
     
-    #sys.exit(0)    
-    #print(trainX['UserID'].value_counts())
-    #print(trainX['MovieID'].value_counts())
-    
-    #sys.exit()
-    
 # =============================================================================
 #   Train
 # =============================================================================
-    best_model_path = 'best.hdf5'
-    
+    begin_time = time.time()
     myModel = gen_model(n_users=n_user, n_items=n_movie, latent_dim=paraDict['latent_dim'])
     trainX, validX = train_test_split(trainX, test_size=0.1)
     
     myEarlyStop      = callbacks.EarlyStopping(monitor='val_loss', patience=3, verbose=1, mode='auto')
-    myModelCheckPoint= callbacks.ModelCheckpoint(filepath=best_model_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    myModelCheckPoint= callbacks.ModelCheckpoint(filepath=paraDict['output_model_path'], monitor='val_loss', verbose=1, save_best_only=True, mode='min')
     
     history = myModel.fit(x=[trainX['UserIdx'].values, trainX['MovieIdx'].values], 
                           y=trainX['Rating'], 
@@ -145,10 +117,16 @@ def main(args):
                           shuffle=True,
                           verbose=1,
                           )
+    
+    historyDF = pd.DataFrame(index=np.arange(len(history.history['val_loss'])))
+    historyDF['val_loss'] = pd.Series(history.history['val_loss'])
+    historyDF.to_csv("history.csv")
+    
+    end_time = time.time()
 # =============================================================================
 #   Validation
 # =============================================================================        
-    myModel = models.load_model(best_model_path)
+    myModel = models.load_model(paraDict['output_model_path'])    
     
     score = myModel.evaluate(x=[validX['UserIdx'].values, validX['MovieIdx'].values], 
                              y=validX['Rating'],
@@ -157,50 +135,43 @@ def main(args):
     print("==========================================")
     print("Test MSE Loss: {0:.4f}".format(np.sqrt(score)))
     print("==========================================")
+    print("Training Time: {0:d} sec".format(int(end_time-begin_time)))
+    predict(paraDict)
     
 # =============================================================================
 #   Prediction
 # =============================================================================     
-    #new_id = []
-    print("Start Prediction")
+def predict(paraDict):
+    model = models.load_model(paraDict['output_model_path'])    
     
-    #testX['UserIdx']  = pd.Series(0, index=testX.index)
-    #testX['MovieIdx'] = pd.Series(0, index=testX.index)    
-        
+    print("Start Prediction")        
+    
+    testX      = pd.read_csv(paraDict['test_path'] , index_col=0)        
+    userLE, movieLE = get_usr_movie_idx(paraDict['user_path'], paraDict['movie_path'])        
+    
     testX['UserIdx']  = userLE.transform(testX['UserID'])
     testX['MovieIdx'] = movieLE.transform(testX['MovieID'])
+ 
+
+    predY = model.predict(x=[testX['UserIdx'].values, testX['MovieIdx'].values],
+                          batch_size=paraDict['batch_size'],
+                          verbose=1)
     
-    """
-    newUserIdList  = []
-    newMovieIdList = []
-    for idx, row in testX.iterrows():
-        usr_id   = row['UserID']
-        movie_id = row['MovieID']
-        
-        if usr_id in userDict:
-            testX.loc[idx,'UserIdx']  = usr_id            
-        else:
-            testX.loc[idx,'UserIdx']  = new_user_idx
-            newUserIdList.append(usr_id)
-        
-        if usr_id in userDict:
-            testX.loc[idx,'MovieIdx'] = movie_id
-        else:
-            testX.loc[idx,'MovieIdx'] = new_movie_idx
-            newUserIdList.append(movie_id)
-            
-    newUserIdList  = list(set(newUserIdList))
-    newMovieIdList = list(set(newMovieIdList))
-    
-    print("newUserIdList:\n",newUserIdList)
-    print("newMovieIdList:\n",newMovieIdList)
-    """    
-    predY = myModel.predict(x=[testX['UserIdx'].values, testX['MovieIdx'].values],
-                            batch_size=paraDict['batch_size'])
     
     testX['Rating'] = pd.Series(predY.flatten(), index=testX.index)       
-    testX['Rating'].to_csv("prediction.csv", header=True)
+    testX['Rating'].to_csv("prediction.csv", header=True)  
+    print("Dump prediction.csv")   
     
+
+def get_usr_movie_idx(user_path, movie_path):
+        userDF     = pd.read_csv(user_path, index_col=None, delimiter="::")    
+        movieDF    = pd.read_csv(movie_path, index_col=None, delimiter="::")   
+        userLE  = preprocessing.LabelEncoder()
+        movieLE = preprocessing.LabelEncoder()    
+        userLE.fit(userDF['UserID'])
+        movieLE.fit(movieDF['movieID'])
+        
+        return userLE, movieLE
 # =============================================================================
 #   Main  
 # =============================================================================
@@ -230,8 +201,16 @@ if __name__ == "__main__":
                           default="data/movies.csv",                       
                           dest='movie_path',
                           help='movie_path') 
-        
-    myParser.add_argument('--n_epoch',      type=int,    default=50,    dest='n_epoch',     help='n_epoch')    
+    
+    myParser.add_argument('--output_model_path', 
+                          type=str,                          
+                          default="best.hdf5",                       
+                          dest='output_model_path',
+                          help='output_model_path') 
+    
+    myParser.add_argument('-pred_only',     action='store_true',       dest='pred_only',     help='pred_only')        
+    #myParser.add_argument('-pred_only',     action='store_false',       dest='pred_only',     help='pred_only')        
+    myParser.add_argument('--n_epoch',      type=int,    default=1,    dest='n_epoch',     help='n_epoch')    
     myParser.add_argument('--batch_size',   type=int,    default=512,   dest='batch_size',  help='batch_size')    
     myParser.add_argument('--latent_dim',   type=int,    default=8,     dest='latent_dim',   help='latent_dim')        
     
